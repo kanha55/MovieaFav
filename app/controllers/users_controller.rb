dbbe389c936b7996e3ca41cb0e2798fe2765c002
kanha55 <kanha.dubey9@gmail.com>
2021-03-02 12:11:55 +0530

    Set users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: %i[show destroy]
+  def index
+  	@users = User.all
+  end
+
+  def show; end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_path
+  end
+
+  private
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
+end
